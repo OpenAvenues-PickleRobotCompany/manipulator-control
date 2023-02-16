e8a468d97522102af34e392e22c3aa0178483db0
naderxahmed <71537050+naderxahmed@users.noreply.github.com>
2023-02-16 12:08:51 -0500

    almost finished need to fix discretization

--- src/control/pid.py
+++ src/control/pid.py
@@ -1,16 +1,18 @@
 from enum import Enum
 
+
 class DiscretizationMethod(Enum):
     EULER_FORWARD = 'euler_forward'
     # EULER_IMPLICIT = 'euler_implicit'
-
 
 class P:
     def __init__(self, kp: float):
         self.kp = kp
 
+
     def discretize(self):
         pass
+
 
     def compute_command(self, desired_state: float, current_state: float):
         return self.kp * (desired_state - current_state)
@@ -24,14 +26,30 @@ class PID:
         self.ts = ts
         self.discretization_method = discretization_method
 
+
         self.discretize()
+        self.last_error = 0  #initialize previous error to approximate the error derivative later
+        self.integral_error = 0 #initialize this to zero so it accumulates (riemann)
+
 
     def discretize(self):
-        # TODO: implement
-        pass
-
+        if self.discretization_method == DiscretizationMethod.EULER_FORWARD:
+            self.kp = self.kp / self.ts
+            self.kd = self.kd / self.ts
+            self.ki = self.ki * self.ts #area approximation by multiplying
+           
     def compute_command(self, desired_state: float, current_state: float):
-        pass
+        error = desired_state - current_state
+        deriv_error = (error-self.last_error)/self.ts
+        self.integral_error += error * self.ts
+       
+        self.last_error = error
+
+
+        command = self.kp*error + self.kd*deriv_error + self.ki*self.integral_error
+       
+        return command
+
 
 
 
