1603f36c4a0e1cbb6449008e0257959078244e29
sy z <szhai@mail.ccsf.edu>
2023-02-20 10:48:24 -0800

    update 2
    Add forward_kinematics_2R, need help with discretization and PID implement

--- src/control/pid.py
+++ src/control/pid.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import math as m
 
 class DiscretizationMethod(Enum):
     EULER_FORWARD = 'euler_forward'
@@ -17,26 +18,45 @@ class P:
 
 
 class PID:
-    def __init__(self, kp: float, kd: float, ki: float, ts: float, discretization_method: DiscretizationMethod):
+    def __init__(self, kp: float, ki: float, kd: float, ts: float, discretization_method: DiscretizationMethod):
         self.kp = kp
         self.kd = kd
         self.ki = ki
         self.ts = ts
+        self.max_output = 100
+        self.min_output = -100
         self.discretization_method = discretization_method
 
         self.discretize()
 
     def discretize(self):
-        # TODO: implement
-        pass
+        kp_d = self.kp
+        ki_d = self.ki * self.ts
+        kd_d = self.kd / self.ts
+        pid_d = PID (kp_d, ki_d, kd_d, self.ts)
+        return pid_d
 
     def compute_command(self, desired_state: float, current_state: float):
         error = desired_state - current_state
         p = self.kp * error
-        d = self.kd * error / self.ts
-        i = self.ki * error * self.ts
+        d = self.kd * (error-self.last_error) / self.ts
+        i = self.ki * (error+self.last_error)/2 * self.ts
+        #antiwind?
+        if i > self.max_output:
+            i = self.max_output
+        elif i < self.min_output:
+            i = self.min_output
+
+        self.last_error = error
         return p+d+i
 
 
 if __name__ == '__main__':
     pass
+
+
+def forward_kinematics_2R(theta1, theta2, L1, L2):
+    x = L1*m.cos(theta1) + L2*m.cos(theta1+theta2)
+    y = L1*m.sin(theta1) + L2*m.sin(theta1+theta2)
+    phi = theta1 + theta2
+    return x, y, phi
--- src/control/test/test_pid.py
+++ src/control/test/test_pid.py
@@ -1,5 +1,5 @@
-from src.control.pid import P
-import pytest
+from src.control.pid import P,PID,forward_kinematics_2R
+import math as m
 
 def test_proportional():
     null_controller = P(kp=0.)
@@ -26,3 +26,9 @@ def test_pid():
     new_controller_1 = PID(kp = 1., ki = 2., kd = 3. , ts = 0.1)
     for i in range(10):
         assert i*(1+2/0.1+3/0.1) == new_controller_1.compute_command(i, 0)
+
+def test_forward_kinematics_2R():
+    x,y,phi = forward_kinematics_2R(m.pi/6, m.pi/3, 1, 2)
+    x == 1*m.cos(m.pi/6) + 2*m.cos(m.pi/3 + m.pi/6)
+    y == 1*m.sin(m.pi/6) + 2*m.sin(m.pi/3 + m.pi/6)
+    phi == m.pi/2
