d43bdd8782af13bd4f938712a8f96a1c77baa8e0
naderxahmed <71537050+naderxahmed@users.noreply.github.com>
2023-02-21 10:58:57 -0500

    implemented planar fwd kinematics

--- src/control/pid.py
+++ src/control/pid.py
@@ -1,18 +1,17 @@
 from enum import Enum
-
+import matplotlib.pyplot as plt 
+import numpy as np
 
 class DiscretizationMethod(Enum):
     EULER_FORWARD = 'euler_forward'
-    # EULER_IMPLICIT = 'euler_implicit'
+    EULER_BACKWARD = 'euler_backward' #implicit
 
 class P:
     def __init__(self, kp: float):
         self.kp = kp
-
-
+        
     def discretize(self):
         pass
-
 
     def compute_command(self, desired_state: float, current_state: float):
         return self.kp * (desired_state - current_state)
@@ -26,32 +25,74 @@ class PID:
         self.ts = ts
         self.discretization_method = discretization_method
 
-
         self.discretize()
-        self.last_error = 0  #initialize previous error to approximate the error derivative later
-        self.integral_error = 0 #initialize this to zero so it accumulates (riemann)
+        
+        self.previous_error = 0  
+        self.integral_error = 0 
 
 
     def discretize(self):
         if self.discretization_method == DiscretizationMethod.EULER_FORWARD:
-            self.kp = self.kp / self.ts
             self.kd = self.kd / self.ts
-            self.ki = self.ki * self.ts #area approximation by multiplying
+            self.ki = self.ki * self.ts 
+            
+        
+        if self.discretization_method == DiscretizationMethod.EULER_BACKWARD:
+            pass
            
     def compute_command(self, desired_state: float, current_state: float):
         error = desired_state - current_state
-        deriv_error = (error-self.last_error)/self.ts
+        
+        deriv_error = (error-self.previous_error)/self.ts
+        
         self.integral_error += error * self.ts
-       
-        self.last_error = error
-
-
+        
+        self.previous_error = error
+        
         command = self.kp*error + self.kd*deriv_error + self.ki*self.integral_error
-       
+
         return command
+    
+    
+    
 
-
-
-
+#using image on slide27, project session 2 as reference
+class forward_kinematics_planar:
+    def __init__(self, theta1: float, theta2: float, theta3: float, 
+                 l1:float, l2: float, l3: float):
+        
+        self.theta1=theta1
+        self.theta2=theta2
+        self.theta3=theta3
+        
+        self.l1=l1
+        self.l2=l2
+        self.l3=l3
+        
+        #joint 0=base
+        self.joint1_pos=None 
+        self.joint2_pos=None
+        self.end_effector_pos=None 
+    
+        self.compute_positions()
+        
+    #computes positions relative to joint 0 position (x0,y0)
+    def compute_positions(self):
+        
+        x1=self.l1*np.cos(self.theta1)
+        y1=self.l1*np.sin(self.theta1)
+        self.joint1_pos=(x1,y1) 
+        
+        x2 = x1 + self.l2*np.cos(self.theta1+self.theta2)
+        y2 = y1 + self.l2*np.sin(self.theta1+self.theta2)
+        self.joint2_pos=(x2,y2) 
+        
+        x3 = x2 + self.l3 * np.cos(self.theta1 + self.theta2 + self.theta3)
+        y3 = y2 + self.l3 * np.sin(self.theta1 + self.theta2 + self.theta3)
+        self.end_effector_pos = (x3,y3) 
+        
+    
+    
+    
 if __name__ == '__main__':
     pass
--- src/control/test/test_pid.py
+++ src/control/test/test_pid.py
@@ -1,4 +1,5 @@
-from src.control import *
+from src.control.pid import *
+import numpy
 
 def test_proportional():
     null_controller = P(kp=0.)
@@ -24,16 +25,40 @@ def test_proportional():
 
 def test_pid():
     pid = PID(kp=1,kd=1,ki=1,ts=0.1,discretization_method=DiscretizationMethod.EULER_FORWARD)
-   
+    
+    #expect zero
     command = pid.compute_command(desired_state=0, current_state=0)
     assert 0 == command
-    #expect zero
+
    
+    #expect nonzero command to drive current state upwards
     command = pid.compute_command(desired_state=1, current_state=0)
     assert command > 0
-    #expect nonzero command to drive current state upwards
-   
-    #expect control to increase
-    for i in range(10):
-        command = pid.compute_command(desired_state=i*2, current_state=i)
-        assert abs(command) > 0
+    
+
+def test_pid_plotting():
+    ts=0.01
+    pid = PID(kp=5,kd=.1,ki=.001,ts=ts,discretization_method=DiscretizationMethod.EULER_FORWARD)
+    
+    commands = []
+    desired = []
+
+    current_state=0
+    
+    desired_state=1000
+    
+    while abs(desired_state - current_state) > 0.1:
+        command = pid.compute_command(desired_state=desired_state, current_state=current_state)
+        commands.append(command)
+        current_state += command
+
+    
+    time = [ts*i for i in range(len(commands))]
+    plt.plot(time,commands)
+    plt.legend(["desired","current"])
+    plt.title("PID Commands")
+    plt.xlabel("Time")
+    plt.ylabel("Command")
+    plt.show()
+
+
