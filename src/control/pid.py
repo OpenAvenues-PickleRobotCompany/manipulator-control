3825dfcb1f865d342fcd2b61a28154a0d42e7827
naderxahmed <71537050+naderxahmed@users.noreply.github.com>
2023-02-25 20:43:46 -0500

    fixed discretization, added anti-windup

--- src/control/pid.py
+++ src/control/pid.py
@@ -4,93 +4,83 @@ import numpy as np
 
 class DiscretizationMethod(Enum):
     EULER_FORWARD = 'euler_forward'
-    EULER_BACKWARD = 'euler_backward' #implicit
+    EULER_BACKWARD = 'euler_backward'
+
 
 class P:
     def __init__(self, kp: float):
         self.kp = kp
         
-    def discretize(self):
-        pass
-
     def compute_command(self, desired_state: float, current_state: float):
         return self.kp * (desired_state - current_state)
 
 
 class PID:
-    def __init__(self, kp: float, kd: float, ki: float, ts: float, discretization_method: DiscretizationMethod):
+    def __init__(self, kp: float, kd: float, ki: float, ts: float,discretization_method: DiscretizationMethod):
         self.kp = kp
         self.kd = kd
         self.ki = ki
         self.ts = ts
         self.discretization_method = discretization_method
-
-        self.discretize()
+        
+        self.saturation_limits = (-1000,1000)
         
         self.previous_error = 0  
         self.integral_error = 0 
 
 
-    def discretize(self):
-        if self.discretization_method == DiscretizationMethod.EULER_FORWARD:
-            self.kd = self.kd / self.ts
-            self.ki = self.ki * self.ts 
-            
-        
-        if self.discretization_method == DiscretizationMethod.EULER_BACKWARD:
-            pass
-           
     def compute_command(self, desired_state: float, current_state: float):
-        error = desired_state - current_state
         
-        deriv_error = (error-self.previous_error)/self.ts
+        error = desired_state - current_state #current error
         
-        self.integral_error += error * self.ts
+        self.integral_error += error * self.ts #riennman approximation, error*ts = rectangle. smaller ts:closer to continuous time 
+            
+        deriv_error = (error-self.previous_error) / self.ts 
+        
+        command = self.kp*error + self.kd*deriv_error + self.ki*self.integral_error
+        
+        #Antiwindup: If the actuator is saturated (reached its physical limit), the error will keep accumulating. 
+        #Prevent this by adjusting command if it hits a saturation limit
+        #saturation_limits = (lower_limit, upper_limit)
+        if command < self.saturation_limits[0]:
+            command = self.saturation_limits[0]
+            
+        if command > self.saturation_limits[1]:
+            command = self.saturation_limits[1] 
         
         self.previous_error = error
         
-        command = self.kp*error + self.kd*deriv_error + self.ki*self.integral_error
-
         return command
     
-    
-    
-
 #using image on slide27, project session 2 as reference
 class forward_kinematics_planar:
-    def __init__(self, theta1: float, theta2: float, theta3: float, 
-                 l1:float, l2: float, l3: float):
+    def __init__(self, theta1: float, theta2: float,  
+                 l1:float, l2: float):
         
         self.theta1=theta1
         self.theta2=theta2
-        self.theta3=theta3
-        
+   
         self.l1=l1
         self.l2=l2
-        self.l3=l3
-        
-        #joint 0=base
-        self.joint1_pos=None 
-        self.joint2_pos=None
-        self.end_effector_pos=None 
-    
-        self.compute_positions()
-        
+ 
     #computes positions relative to joint 0 position (x0,y0)
     def compute_positions(self):
         
-        x1=self.l1*np.cos(self.theta1)
-        y1=self.l1*np.sin(self.theta1)
-        self.joint1_pos=(x1,y1) 
+        x1 = self.l1*np.cos(self.theta1)
+        y1 = self.l1*np.sin(self.theta1)
+        joint1_pos = (x1,y1) 
         
         x2 = x1 + self.l2*np.cos(self.theta1+self.theta2)
         y2 = y1 + self.l2*np.sin(self.theta1+self.theta2)
-        self.joint2_pos=(x2,y2) 
+        end_effector_pos = (x2,y2)
         
-        x3 = x2 + self.l3 * np.cos(self.theta1 + self.theta2 + self.theta3)
-        y3 = y2 + self.l3 * np.sin(self.theta1 + self.theta2 + self.theta3)
-        self.end_effector_pos = (x3,y3) 
-        
+        return joint1_pos, end_effector_pos
+    
+# class inverse_kinematics_planar: 
+class inverse_kinematics_planar:
+    def __init__(self, end_effector_pos:float, )
+    
+
     
     
     
